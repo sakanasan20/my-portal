90236b93129341b15f5dd38171ae879aa60a6b84
Nick Chen <nick.chen@cmp.com.tw>
2025-05-08 09:11:25 +0800

    Initial commit

---
+++ src/main/java/com/niq_dev/portal/MyPortalApplication.java
@@ -0,0 +1,13 @@
+package com.niq_dev.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPortalApplication.class, args);
+	}
+
+}
